462ed7b74eda1176bf6eb1c12150e013df065393
Guilherme_Tamega <guitamega06@gmail.com>
2022-02-23 10:34:24 -0300

    Vercel upload attempt - 2

--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -7,6 +7,7 @@ import AudioPlayer from "../../components/AudioPlayer";
 import Image from "next/image";
 
 import * as S from "./styles";
+import { useRouter } from "next/router";
 
 type ImageProps = {
   url: string;
@@ -15,38 +16,46 @@ type ImageProps = {
 };
 
 export type PageTemplateProps = {
-  heading: string;
-  body: string;
-  title: string;
-  gallery: ImageProps[];
+  page: {
+    heading: string;
+    body: string;
+    title: string;
+    gallery: ImageProps[];
+  };
 };
 
-const PageTemplate = ({ heading, body, title, gallery }: PageTemplateProps) => (
-  <>
-    <PageTitle />
-    <Title src="/home.png" href="/">
-      ★{title}★
-    </Title>
-    <TextBox>
-      <P>{heading}</P>
-      <div dangerouslySetInnerHTML={{ __html: body }} />
-    </TextBox>
-    <S.Gallery>
-      {gallery.map((image, index) => (
-        <Image
-          key={`photo-${index}`}
-          src={image.url}
-          alt={`photo-${index}`}
-          width={image.width}
-          height={image.height}
-          quality={100}
-          layout="responsive"
-          priority
-        />
-      ))}
-    </S.Gallery>
-    <AudioPlayer src="/audios/audioOgg.ogg" />
-  </>
-);
+function PageTemplate({ page }: PageTemplateProps) {
+  const router = useRouter();
+
+  if (router.isFallback) return null;
+
+  return (
+    <>
+      <PageTitle />
+      <Title src="/home.png" href="/">
+        ★{page.title}★
+      </Title>
+      <TextBox>
+        <P>{page.heading}</P>
+        <div dangerouslySetInnerHTML={{ __html: page.body }} />
+      </TextBox>
+      <S.Gallery>
+        {page.gallery.map((image, index) => (
+          <Image
+            key={`photo-${index}`}
+            src={image.url}
+            alt={`photo-${index}`}
+            width={image.width}
+            height={image.height}
+            quality={100}
+            layout="responsive"
+            priority
+          />
+        ))}
+      </S.Gallery>
+      <AudioPlayer src="/audios/audioOgg.ogg" />
+    </>
+  );
+}
 
 export default PageTemplate;
